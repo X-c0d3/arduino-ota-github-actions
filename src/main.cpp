d6f50e96c7b178befa74a16346819ffcddefeefa
Watchara Pongsri <giffyp@hotmail.com>
2021-08-05 01:03:31 +0700

    refactor code

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 
 #include "configs.h"
 #include "firmware.h"
+#include "makeResponse.h"
 #include "utility.h"
 #include "wifiMan.h"
 
@@ -65,6 +66,54 @@ void event(uint8_t *payload, size_t length) {
             previousMillis = currentMillis;
         }
     }
+}
+
+String jsonResult;
+bool readData(void *) {
+    if (firmwareUpgradeProgress > 0)  //Kill task when firmware start upgrading
+        return true;
+
+    float DC_VOLTAGE, DC_CURRENT, DC_POWER, DC_ENERGY, AC_VOLTAGE, AC_CURRENT, AC_POWER, AC_ENERGY, AC_FREQUENCY, AC_PF, HUMIDITY, TEMPERATURE;
+    // For testing
+    if (ENABLE_TEST_MODE) {
+        DC_VOLTAGE = random(350, 400);
+        DC_CURRENT = random(5, 7);
+        DC_POWER = random(90, 200);
+        HUMIDITY = random(50, 100);
+        TEMPERATURE = random(20, 50);
+
+        AC_VOLTAGE = random(200, 250);
+        AC_CURRENT = random(8, 15);
+        AC_POWER = random(300, 400);
+        AC_FREQUENCY = random(49, 54);
+    }
+
+    if (ENABLE_DEBUG_MODE) {
+        Serial.println("########### PZEM-017 ###############");
+        Serial.println("PZEM-017 V: " + String(DC_VOLTAGE));
+        Serial.println("PZEM-017 A: " + String(DC_CURRENT));
+        Serial.println("PZEM-017 W:" + String(DC_POWER));
+        Serial.println("PZEM-017 E: " + String(DC_ENERGY));
+
+        Serial.println("########### PZEM-014 ###############");
+        Serial.println("PZEM-004T VOLTAGE: " + String(AC_VOLTAGE));
+        Serial.println("PZEM-004T CURRENT: " + String(AC_CURRENT));
+        Serial.println("PZEM-004T POWER:" + String(AC_POWER));
+        Serial.println("PZEM-004T ENERGY: " + String(AC_ENERGY));
+        Serial.println("PZEM-004T FREQUENCY: " + String(AC_FREQUENCY));
+        Serial.println("PZEM-004T PF: " + String(AC_PF));
+        Serial.println("FIRMWARE_VERSION: " + String(FIRMWARE_VERSION));
+    }
+
+    jsonResult = createResponse(webSocket, DC_VOLTAGE, DC_CURRENT, DC_POWER, DC_ENERGY,            //DC
+                                AC_VOLTAGE, AC_CURRENT, AC_POWER, AC_ENERGY, AC_FREQUENCY, AC_PF,  //AC
+                                HUMIDITY, TEMPERATURE);
+
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    timerWrite(watchdogTimer, 0);  // reset timer feed dog
+
+    return true;
 }
 
 void interruptReboot() {
@@ -149,6 +198,8 @@ void setup() {
         webSocket.begin(SOCKETIO_HOST, String(SOCKETIO_PORT).toInt());
         webSocket.onEvent(socketIOEvent);
         webSocket.setReconnectInterval(5000);
+
+        timer.every(DEFAULT_INTERVAL, readData);
 
         if (ENABLE_FIRMWARE_AUTOUPDATE)
             timer.every(CHECK_FIRMWARE_INTERVAL, firmwareUpdate);
